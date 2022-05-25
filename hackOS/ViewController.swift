8793217038831f664d1ad1cf00210f2527009fd8
Isaac Lopez <88524927+GIsaacLN@users.noreply.github.com>
2022-05-25 12:50:37 -0500

    Botones
    Agregar botones

--- hackOS/ViewController.swift
+++ hackOS/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var lengua: UIMenu!
+    
+    @IBOutlet weak var texto: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
