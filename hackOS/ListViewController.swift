211908ed8c2e04528d7b58e2a6917801659a87a6
Raymundo <92004981+Raxel99-UNAM@users.noreply.github.com>
2022-05-26 13:32:01 -0700

    Ya jaló

--- hackOS/ListViewController.swift
+++ hackOS/ListViewController.swift
@@ -7,15 +7,42 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource {
+    
     @IBOutlet weak var tblList: UITableView!
 
+    var names = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tblList.tableFooterView = UIView.init(frame: .zero)
+        tblList.dataSource = self
 
         // Do any additional setup after loading the view.
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if let lbl = cell?.contentView.viewWithTag(101) as? UILabel {
+            lbl.text = names[indexPath.row]
+        }
+        if let btnDelete = cell?.contentView.viewWithTag(102) as? UIButton {
+            btnDelete.addTarget(self, action: #selector(deleteRow(_ :)), for: .touchUpInside)
+        }
+        return cell!
+    }
+    
+    @IBAction func addRow(_ sender: UIButton){
+        
+    }
+    
+    @objc func deleteRow(_ sender: UIButton){
+        
+    }
 
     /*
     // MARK: - Navigation
