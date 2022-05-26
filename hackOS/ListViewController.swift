c1d8fb24311b35f7049aedb8f6a51d05b5806941
Raymundo <92004981+Raxel99-UNAM@users.noreply.github.com>
2022-05-26 08:48:40 -0700

    Puse la lista, aun falta. Isaac no trajo a los bailarines

---
+++ hackOS/ListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListViewController.swift
+//  hackOS
+//
+//  Created by iOS UNAM 07 on 26/05/22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    @IBOutlet weak var tblList: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
